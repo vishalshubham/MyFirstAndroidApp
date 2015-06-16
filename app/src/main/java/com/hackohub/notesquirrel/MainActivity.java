50bee4e558d606f73268ace8583553f73a48b4a8
vishalshubham <vishal.shubham@gmail.com>
2015-06-15 20:47:23 -0400

    More Changes

--- app/src/main/java/com/hackohub/notesquirrel/MainActivity.java
+++ app/src/main/java/com/hackohub/notesquirrel/MainActivity.java
@@ -1,28 +1,76 @@
 package com.hackohub.notesquirrel;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String DEBUGTAG = "VC";
+    public static final String TEXTFILE = "NoteSquirrel.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButtonListner();
         setContent();
     }
 
-    public void setContent(){
-//        EditText t = (EditText)findViewById(R.id.username);
-//        t.setGravity(Gravity.CENTER);
+    public void loginButtonListner(){
+        Button saveBtn = (Button)findViewById(R.id.login);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-//        EditText t1 = (EditText)findViewById(R.id.password);
-//        t1.setGravity(Gravity.CENTER);
+                EditText editText = (EditText)findViewById(R.id.username);
+                String text = editText.getText().toString();
+
+                try {
+                    FileOutputStream fos = openFileOutput(TEXTFILE, Context.MODE_PRIVATE);
+                    fos.write(text.getBytes());
+                    fos.close();
+                } catch (Exception e) {
+                    Log.d(DEBUGTAG, "Unable to save file" + e);
+                }
+                Log.d(DEBUGTAG, "Login Button Clicked" + text);
+            }
+        });
+    }
+
+
+    public void setContent(){
+        try {
+            FileInputStream fis = openFileInput(TEXTFILE);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new DataInputStream(fis)));
+
+            String line;
+            EditText editText = (EditText)findViewById(R.id.username);
+
+            while ((line = reader.readLine())!=null){
+                editText.append(line);
+            }
+            fis.close();
+        } catch (Exception e) {
+            Log.d(DEBUGTAG, "Unable to read file" + e);
+        }
     }
 
     @Override
