ca1b69bf3a26df22ec6acbc4d138327e3c5a485e
vishalshubham <vishal.shubham@gmail.com>
2015-07-30 14:00:00 +0100

    Test commit

--- app/src/main/java/com/hackohub/notesquirrel/ImageActivity.java
+++ app/src/main/java/com/hackohub/notesquirrel/ImageActivity.java
@@ -20,7 +20,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.List;
+import java.util.List; 
 
 
 public class ImageActivity extends ActionBarActivity implements PointCollectorListner{
